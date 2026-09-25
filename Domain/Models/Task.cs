6354ed988586b155906fb32b63ca5f247b1d94ce
Kingigor <103347909+K1ngG0r@users.noreply.github.com>
2026-09-25 09:32:08 +0700

    Update Task.cs

--- Domain/Models/Task.cs
+++ Domain/Models/Task.cs
@@ -7,6 +7,8 @@ public class Task
     public string ShortDescription { get; set; }
     public string Description { get; set; }
 
+    public Guid UserId { get; set}
+
     public string Status { get; set; } 
 
     public Task() { }
